f97fdff0d42618874f8d65e4a3804fc32989ed8f
Jmestrelozano <jmestrelozano@gmail.com>
2023-10-15 12:03:17 -0500

    Refactored DetailsFitScreen component for improved functionality and readability

--- src/Screens/DetailsFit/DetailsFitScreen.tsx
+++ src/Screens/DetailsFit/DetailsFitScreen.tsx
@@ -1,111 +1,118 @@
-import { Text, TouchableOpacity, View } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import {Text, TouchableOpacity, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import FastImage from 'react-native-fast-image';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { RootStackParamList } from '../../Navigations/StackNavigator';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { styles } from './stylesheetDetailsFit';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {RootStackParamList} from '../../Navigations/StackNavigator';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {
+  setExercisesCompleted,
+  setInitialIndexScreen,
+} from '../../Store/Slices/fitSlices';
+import {storeInterface} from '../../Store/store';
+
 import {
   dimensions,
   useAppDispatch,
   useAppSelector,
 } from '../../Globales/globales';
-import {
-  setExercisesCompleted,
-  setInitialIndexScreen,
-} from '../../Store/Slices/fitSlices';
-import { storeInterface } from '../../Store/store';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {styles} from './stylesheetDetailsFit';
 
 export type Props = NativeStackScreenProps<RootStackParamList, 'DetailFit'>;
 
-export const DetailsFitScreen = ({ route, navigation }: Props) => {
+export const DetailsFitScreen = ({route, navigation}: Props) => {
   const dispatch = useAppDispatch();
   const {
     initialIndexScreen,
-    categoryExercise: { exercises, nameCategory, id: idCompletedExercise },
+    categoryExercise: {exercises, nameCategory, id: idCompletedExercise},
     exercisesCompleted,
   } = useAppSelector((store: storeInterface) => store.fit);
 
   const [isCompleted, setIsCompleted] = useState<boolean>(false);
-  console.log(initialIndexScreen, exercises)
-  const { name, image, sets, id } = exercises[initialIndexScreen];
+  const [secExercise, setSecExercise] = useState<number>(0);
+  const {name, image, sets, id} = exercises[initialIndexScreen];
 
-  const { hp } = dimensions;
+  const {hp} = dimensions;
 
   useEffect(() => {
-    const isFinished = (exercises.length - 1) === initialIndexScreen;
+    const isFinished = exercises.length - 1 === initialIndexScreen;
 
     if (isFinished) {
       setIsCompleted(isFinished);
-      return
-    };
+      return;
+    }
 
-    setIsCompleted(false)
-
+    setIsCompleted(false);
   }, [initialIndexScreen]);
 
   const isDuplicate = exercisesCompleted[idCompletedExercise].completed.some(
-    item => item.id === id && item.status === 'filled'
+    item => item.id === id && item.status === 'filled',
   );
 
+  const onCompletedExercise = (isReplace?: boolean): void => {
+    if (!isDuplicate) {
+      dispatch(
+        setExercisesCompleted({
+          index: initialIndexScreen + 1,
+          type: nameCategory,
+          data: {id, status: 'filled', secExercise},
+        }),
+      );
+
+      if (isReplace) {
+        navigation.replace('Home');
+        return;
+      }
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSecExercise(prevSec => prevSec + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <FastImage style={styles.cardImage}
-        source={{ uri: image, priority: FastImage.priority.normal, cache: FastImage.cacheControl.immutable }}
+    <SafeAreaView style={{flex: 1}}>
+      <FastImage
+        style={styles.cardImage}
+        source={{
+          uri: image,
+          priority: FastImage.priority.normal,
+          cache: FastImage.cacheControl.immutable,
+        }}
         resizeMode={FastImage.resizeMode.stretch}
       />
       <View style={styles.container}>
         <View style={styles.col_1}>
           <Text style={styles.cardTitle}>{name}</Text>
-          <Text style={{ ...styles.cardTitle, marginTop: hp(2), fontSize: 25 }}>
+          <Text style={{...styles.cardTitle, marginTop: hp(2), fontSize: 25}}>
             x{sets}
           </Text>
 
-
           {isCompleted ? (
             <TouchableOpacity
-
               onPress={() => {
-                if (!isDuplicate) {
-                  dispatch(
-                    setExercisesCompleted({
-                      index: initialIndexScreen + 1,
-                      type: nameCategory,
-                      data: { id, status: 'filled' },
-                    }),
-                  );
-                  navigation.replace('Home');
-                  return;
-                }
+                onCompletedExercise(true);
                 navigation.replace('Home');
               }}
               style={styles.button}>
-              <Text style={styles.textButton}>Finalizar</Text>
+              <Text style={styles.textButton}>Complete</Text>
             </TouchableOpacity>
           ) : (
             <TouchableOpacity
               onPress={() => {
-                // Comprobar si los datos ya están en la matriz completed
-
-                if (!isDuplicate) {
-                  dispatch(
-                    setExercisesCompleted({
-                      index: initialIndexScreen + 1,
-                      type: nameCategory,
-                      data: { id, status: 'filled' },
-                    }),
-                  );
-                }
-
+                onCompletedExercise();
                 dispatch(setInitialIndexScreen(initialIndexScreen + 1));
                 navigation.replace('BreakExercise');
               }}
               style={styles.button}>
-              <Text style={styles.textButton}>Siguiente</Text>
+              <Text style={styles.textButton}>Next</Text>
             </TouchableOpacity>
           )}
-
         </View>
 
         {initialIndexScreen > 0 && (
@@ -116,26 +123,28 @@ export const DetailsFitScreen = ({ route, navigation }: Props) => {
                 navigation.replace('DetailFit');
               }}
               style={styles.buttonNavigation}>
-              <Text style={styles.textNavigation}>Anterior</Text>
+              <Text style={styles.textNavigation}>Previous</Text>
               <Icon name="skip-previous" color="grey" size={30} />
             </TouchableOpacity>
 
-            {!isCompleted ?
+            {!isCompleted ? (
               <>
-                <Text style={{ color: 'grey', alignSelf: 'center' }}>|</Text>
+                <Text style={{color: 'grey', alignSelf: 'center'}}>|</Text>
                 <TouchableOpacity
                   onPress={() => {
+                    onCompletedExercise();
+
                     dispatch(setInitialIndexScreen(initialIndexScreen + 1));
                     navigation.replace('BreakExercise');
                   }}
                   style={styles.buttonNavigation}>
-                  <Text style={styles.textNavigation}>Omitir</Text>
+                  <Text style={styles.textNavigation}>Skip</Text>
                   <Icon name="skip-next" color="grey" size={30} />
                 </TouchableOpacity>
-              </> :
+              </>
+            ) : (
               <></>
-            }
-
+            )}
           </View>
         )}
       </View>
