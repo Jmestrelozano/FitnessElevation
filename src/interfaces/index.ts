2908d8ecc37cc8aca8a5933ce2503f1968de9144
Jmestrelozano <jmestrelozano@gmail.com>
2023-10-15 11:59:52 -0500

    added new interface profileSlice

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,5 @@
 export * from './InterfacesComponents/InterfaceItemHeader';
 export * from './InterfacesServices/InterfaceCategories';
 export * from './InterfacesSlices/InterfaceFit.slice';
+export * from './InterfacesSlices/interfaceProfile.slice';
 export * from './interfaceGlobals/interfaceGlobal';
